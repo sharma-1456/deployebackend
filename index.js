b9079eb12104e12ca0cbdcd2176185b7dc608df9
vishalsharma74880@gmail.com <vishalsharma74880@gmail.com>
2024-05-31 13:50:03 +0530

    githubdata add

--- index.js
+++ index.js
@@ -2,6 +2,40 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 5000
+const githubdata={
+  "login": "sharma-1456",
+  "id": 121045277,
+  "node_id": "U_kgDOBzcBHQ",
+  "avatar_url": "https://avatars.githubusercontent.com/u/121045277?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/sharma-1456",
+  "html_url": "https://github.com/sharma-1456",
+  "followers_url": "https://api.github.com/users/sharma-1456/followers",
+  "following_url": "https://api.github.com/users/sharma-1456/following{/other_user}",
+  "gists_url": "https://api.github.com/users/sharma-1456/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/sharma-1456/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/sharma-1456/subscriptions",
+  "organizations_url": "https://api.github.com/users/sharma-1456/orgs",
+  "repos_url": "https://api.github.com/users/sharma-1456/repos",
+  "events_url": "https://api.github.com/users/sharma-1456/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/sharma-1456/received_events",
+  "type": "User",
+  "site_admin": false,
+  "name": "Vishal Kumar Sharma",
+  "company": null,
+  "blog": "",
+  "location": null,
+  "email": null,
+  "hireable": null,
+  "bio": null,
+  "twitter_username": null,
+  "public_repos": 16,
+  "public_gists": 0,
+  "followers": 0,
+  "following": 1,
+  "created_at": "2022-12-19T15:42:14Z",
+  "updated_at": "2024-05-19T07:54:55Z"
+}
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -12,6 +46,9 @@ app.get('/twitter',(req, res)=>{
 })
 app.get('/youtube', (req, res)=>{
   res.send("hello chai aur code")
+})
+app.get('/github',(req, res)=>{
+      res.json(githubdata)
 })
 
 app.listen(process.env.PORT, () => {
